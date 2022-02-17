114f907cb8081689e31a06d3b39efbf3e1b95d2e
BENcao318 <labber@DESKTOP-U16DFJJ.localdomain>
2022-02-16 21:34:35 -0500

    refactor for clean code.

--- spinner2.js
+++ spinner2.js
@@ -1,9 +1,8 @@
-let spin = ['|', '/', '-', '\\', '/', '-'];
+let spin = ["|", "/", "-", "\\", "/", "-"];
 let delay = 0;
 spin.forEach((char) => {
   setTimeout(() => {
     process.stdout.write(`\r${char}   ${delay}`);
-  }, delay); 
+  }, delay);
   delay += 200;
-})
-
+});
